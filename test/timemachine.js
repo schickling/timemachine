d37c5ea3ff7854a233656dbaa58125bd85f0292c
Johannes Schickling <schickling.j@gmail.com>
2013-09-26 19:05:10 +0200

    First tests

--- test/timemachine.js
+++ test/timemachine.js
@@ -1,7 +1,14 @@
 describe("check timemachine", function () {
 
-  it("should ...", function () {
-    window.timemachine;
+  it("should default to December 24, 1990 12:25:00", function () {
+    expect(new Date().toUTCString()).toBe('Mon, 24 Dec 1990 11:25:00 GMT');
+  });
+
+  it("should now be set to December 25, 1991 13:12:59", function () {
+    timemachine.config({
+      dateString: 'December 25, 1991 13:12:59'
+    });
+    expect(new Date().toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
   });
 
 });
