defadbc8a590c5ac39ab0847cef26fe496776e6a
Johannes Schickling <schickling.j@gmail.com>
2015-01-09 18:07:45 +0100

    fix deps

--- test/timemachine.js
+++ test/timemachine.js
@@ -1,132 +1,133 @@
 describe('check timemachine', function() {
 
-  beforeEach(function() {
-    jasmine.Clock.useMock();
-  });
-
-  afterEach(function() {
-    timemachine.reset();
-    timemachine.config({});
-  });
-
-  describe('check Date constructor', function() {
-
-    it('should default to Thu, 01 Jan 1970 00:00:00 GMT', function() {
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+    beforeEach(function() {
+        jasmine.clock().install();
     });
 
-    it('should now be set to December 25, 1991 13:12:59 via dateString', function() {
-      timemachine.config({
-        dateString: 'December 25, 1991 13:12:59'
-      });
-      expect(new Date().toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
+    afterEach(function() {
+        jasmine.clock().uninstall();
+        timemachine.reset();
+        timemachine.config({});
     });
 
-    it('should now be set to December 25, 1991 13:12:59 via timestamp', function() {
-      timemachine.config({
-        timestamp: 693663179000
-      });
-      expect(new Date().toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
+    describe('check Date constructor', function() {
+
+        it('should default to Thu, 01 Jan 1970 00:00:00 GMT', function() {
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+        });
+
+        it('should now be set to December 25, 1991 13:12:59 via dateString', function() {
+            timemachine.config({
+                dateString: 'December 25, 1991 13:12:59'
+            });
+            expect(new Date().toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
+        });
+
+        it('should now be set to December 25, 1991 13:12:59 via timestamp', function() {
+            timemachine.config({
+                timestamp: 693663179000
+            });
+            expect(new Date().toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
+        });
+
+        it('should stay the same for instantiation with millisecond param', function() {
+            expect(new Date(1).toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+        });
+
+        it('should stay the same for instantiation with dateString', function() {
+            expect(new Date('December 25, 1991 13:12:59').toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
+        });
+
+        it('should stay the same for instantiation with year, month, day, hours, minutes, seconds, milliseconds', function() {
+            expect(new Date(1990, 12, 25, 12, 12, 59, 1).toUTCString()).toBe('Fri, 25 Jan 1991 11:12:59 GMT');
+        });
+
+        it('should reset', function() {
+            timemachine.reset();
+            expect(new Date().toUTCString()).not.toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+        });
+
+        it('should add a time difference as milliseconds', function() {
+            timemachine.config({
+                difference: 10000 // 10 seconds
+            });
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:10 GMT');
+        });
+
+        it('should not tick by default', function() {
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+            jasmine.clock().tick(1000);
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+        });
+
+        it('should tick when enabled', function() {
+            timemachine.config({
+                tick: true
+            });
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+            sleep(1000);
+            expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:01 GMT');
+        });
+
+        it('should keep system time', function() {
+            timemachine.config({
+                keepTime: true
+            });
+            var now = new Date.OriginalDate().toUTCString();
+            expect(new Date().toUTCString()).toBe(now);
+        });
+
     });
 
-    it('should stay the same for instantiation with millisecond param', function() {
-      expect(new Date(1).toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
+    describe('check Date.now()', function() {
+
+        it('should return adjusted timestamp', function() {
+            expect(Date.now()).toBe(0);
+        });
+
+        it('should reset', function() {
+            var now = Date.OriginalDate.now();
+            timemachine.reset();
+            expect(Date.now()).toBe(now);
+        });
+
+        it('should add a time difference as milliseconds', function() {
+            timemachine.config({
+                difference: 10000 // 10 seconds
+            });
+            expect(Date.now()).toBe(10000);
+        });
+
+        it('should not tick by default', function() {
+            expect(Date.now()).toBe(0);
+            sleep(1000);
+            expect(Date.now()).toBe(0);
+        });
+
+        it('should tick when enabled', function() {
+            timemachine.config({
+                tick: true
+            });
+            expect(Date.now()).toBe(0);
+            sleep(1000);
+            expect(Date.now()).toBe(1000);
+        });
+
+        it('should keep system time', function() {
+            timemachine.config({
+                keepTime: true
+            });
+            var now = Date.OriginalDate.now();
+            expect(Date.now()).toBe(now);
+        });
+
     });
-
-    it('should stay the same for instantiation with dateString', function() {
-      expect(new Date('December 25, 1991 13:12:59').toUTCString()).toBe('Wed, 25 Dec 1991 12:12:59 GMT');
-    });
-
-    it('should stay the same for instantiation with year, month, day, hours, minutes, seconds, milliseconds', function() {
-      expect(new Date(1990, 12, 25, 12, 12, 59, 1).toUTCString()).toBe('Fri, 25 Jan 1991 11:12:59 GMT');
-    });
-
-    it('should reset', function() {
-      timemachine.reset();
-      expect(new Date().toUTCString()).not.toBe('Thu, 01 Jan 1970 00:00:00 GMT');
-    });
-
-    it('should add a time difference as milliseconds', function() {
-      timemachine.config({
-        difference: 10000 // 10 seconds
-      });
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:10 GMT');
-    });
-
-    it('should not tick by default', function() {
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
-      jasmine.Clock.tick(1000);
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
-    });
-
-    it('should tick when enabled', function() {
-      timemachine.config({
-        tick: true
-      });
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:00 GMT');
-      sleep(1000);
-      expect(new Date().toUTCString()).toBe('Thu, 01 Jan 1970 00:00:01 GMT');
-    });
-
-    it('should keep system time', function() {
-      timemachine.config({
-        keepTime: true
-      });
-      var now = new Date.OriginalDate().toUTCString();
-      expect(new Date().toUTCString()).toBe(now);
-    });
-
-  });
-
-  describe('check Date.now()', function() {
-
-    it('should return adjusted timestamp', function() {
-      expect(Date.now()).toBe(0);
-    });
-
-    it('should reset', function() {
-      var now = Date.OriginalDate.now();
-      timemachine.reset();
-      expect(Date.now()).toBe(now);
-    });
-
-    it('should add a time difference as milliseconds', function() {
-      timemachine.config({
-        difference: 10000 // 10 seconds
-      });
-      expect(Date.now()).toBe(10000);
-    });
-
-    it('should not tick by default', function() {
-      expect(Date.now()).toBe(0);
-      sleep(1000);
-      expect(Date.now()).toBe(0);
-    });
-
-    it('should tick when enabled', function() {
-      timemachine.config({
-        tick: true
-      });
-      expect(Date.now()).toBe(0);
-      sleep(1000);
-      expect(Date.now()).toBe(1000);
-    });
-
-    it('should keep system time', function() {
-      timemachine.config({
-        keepTime: true
-      });
-      var now = Date.OriginalDate.now();
-      expect(Date.now()).toBe(now);
-    });
-
-  });
 
 });
 
 function sleep(ms) {
-  var start = new Date.OriginalDate().getTime(),
-    expire = start + ms;
-  while (new Date.OriginalDate().getTime() < expire) {}
-  return;
+    var start = new Date.OriginalDate().getTime(),
+        expire = start + ms;
+    while (new Date.OriginalDate().getTime() < expire) {}
+    return;
 }
--- timemachine.js
+++ timemachine.js
@@ -1,4 +1,4 @@
-(function (name, definition) {
+(function(name, definition) {
   if (typeof define === 'function') { // AMD
     define(definition);
   } else if (typeof module !== 'undefined' && module.exports) { // Node.js
@@ -7,50 +7,89 @@
     var timemachine = definition(),
       global = this,
       old = global[name];
-    timemachine.noConflict = function () {
+    timemachine.noConflict = function() {
       global[name] = old;
       return timemachine;
     };
     global[name] = timemachine;
   }
-})('timemachine', function () {
+})('timemachine', function() {
 
   var OriginalDate = Date,
     Timemachine = {
 
-      dateString: 'Wed Dec 25 1991 13:12:59 GMT',
+      timestamp: 0,
+      tick: false,
+      tickStartDate: null,
+      keepTime: false,
+      difference: 0,
 
-      apply: function () {
-        var self = this;
-        Date = function () {
-          if (arguments.length === 1) {
-            return new OriginalDate(arguments[0]);
-          } else if (arguments.length === 7) {
-            return new OriginalDate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
-          } else {
-            return new OriginalDate(self.dateString);
-          }
-        };
-        Date.prototype = OriginalDate.prototype;
-        Date.now = function () {
-          var date = new OriginalDate(self.dateString);
-          return date.getTime();
-        };
-      },
-
-      config: function (options) {
-        this.dateString = options.dateString || this.dateString;
-        this.apply();
+      config: function(options) {
+        this.timestamp = OriginalDate.parse(options.dateString) || options.timestamp || this.timestamp;
+        this.difference = options.difference || this.difference;
+        this.keepTime = options.keepTime || this.keepTime;
+        this.tick = options.tick || this.tick;
+        if (this.tick) {
+          this.tickStartDate = new OriginalDate();
+        }
+        this._apply();
       },
 
       reset: function() {
+        this.timestamp = 0;
+        this.tick = false;
+        this.tickStartDate = null;
+        this.keepTime = false;
+        this.difference = 0;
         Date = OriginalDate;
         Date.prototype = OriginalDate.prototype;
-      }
+      },
+
+      _apply: function() {
+        var self = this;
+
+        Date = function() {
+          var date;
+          if (self.keepTime) {
+            date = new OriginalDate();
+          } else if (arguments.length === 1) {
+            date = new OriginalDate(arguments[0]);
+          } else if (arguments.length === 7) {
+            date = new OriginalDate(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
+          } else {
+            date = new OriginalDate(self.timestamp);
+          }
+
+          var difference = self._getDifference();
+          if (difference !== 0) {
+            date = new OriginalDate(date.getTime() + difference);
+          }
+
+          return date;
+        };
+
+        Date.prototype = OriginalDate.prototype;
+        Date.now = function() {
+          var timestamp = self.keepTime ? OriginalDate.now() : self.timestamp;
+          return timestamp + self._getDifference();
+        };
+        Date.OriginalDate = OriginalDate;
+
+      },
+
+      _getDifference: function() {
+        var difference = this.difference;
+
+        if (this.tick) {
+          difference += OriginalDate.now() - this.tickStartDate.getTime();
+        }
+
+        return difference;
+      },
 
     };
 
-  Timemachine.apply();
+  Timemachine._apply();
 
   return Timemachine;
 
